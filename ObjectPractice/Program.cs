57d17cd10f57332b3eaa0862d308e0d7b96843bb
SeedPaths-User <lessdismissive@email.com>
2014-04-25 15:23:47 -0600

    sturff

--- ObjectPractice/Program.cs
+++ ObjectPractice/Program.cs
@@ -10,14 +10,22 @@ namespace ObjectPractice
     {
         static void Main(string[] args)
         {
-            var england = new Sword("claymore", "two-handed", "English");
-            Console.WriteLine("The " + england.SwordName + " is a lightweight " + england.SwordClass + england.CountryOrigin + " blade.");
+            //var england = new Sword("claymore", "two-handed", "English");
+            //Console.WriteLine("The " + england.SwordName + " is a lightweight " + england.SwordClass + england.CountryOrigin + " blade.");
 
-            var germany = new Sword("zweihander", "two-handed", "German");
-            Console.WriteLine("The " + germany.SwordName + " is a heavy " + germany.SwordClass + germany.CountryOrigin + " blade.");
+            //var germany = new Sword("zweihander", "two-handed", "German");
+            //Console.WriteLine("The " + germany.SwordName + " is a heavy " + germany.SwordClass + germany.CountryOrigin + " blade.");
 
-            var turkey = new Sword("tulwar", "one-handed", "Turkish");
-            Console.WriteLine("The " + turkey.SwordName + " is a medium weight " + turkey.SwordClass + turkey.CountryOrigin + " blade.");
+            //var turkey = new Sword("tulwar", "one-handed", "Turkish");
+            //Console.WriteLine("The " + turkey.SwordName + " is a medium weight " + turkey.SwordClass + turkey.CountryOrigin + " blade.");
+
+            
+            VideoGame EverQuest = new VideoGame("Everquest", "MMORPG", "We got code here!");
+            EverQuest.Play();
+            EverQuest.Awesomeness = 9;
+            EverQuest.Rating = "T";
+            EverQuest.Code = "Better code!";
+            EverQuest.Play();
 
             Console.ReadKey();
         }
---
+++ ObjectPractice/VideoGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class VideoGame
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Awesomeness { get; set; }
+        public string Rating { get; set; }
+        public string Code { get; set; }
+
+        public VideoGame(string title, string genre, string code)
+        {
+            this.Title = title;
+            this.Genre = genre;
+            this.Code = code;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine(this.Code);
+        }
+    }
+}

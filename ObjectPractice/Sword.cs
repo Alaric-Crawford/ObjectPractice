8f3b8e12402c08021012da6eaf1728dcec265034
SeedPaths-User <lessdismissive@email.com>
2014-04-25 14:20:38 -0600

    far better now

--- ObjectPractice/Sword.cs
+++ ObjectPractice/Sword.cs
@@ -11,6 +11,10 @@ namespace ObjectPractice
         public string SwordName { get; set; }
         public string SwordClass { get; set; }
 
-
+        public Sword(string title, string type)
+        {
+            this.SwordName = title;
+            this.SwordClass = type;
+        }
     }
 }

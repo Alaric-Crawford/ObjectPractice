cb35da5ab0532d351a1b37b0d110b16f17092d85
SeedPaths-User <lessdismissive@email.com>
2014-04-25 13:51:58 -0600

    It may cut you...

---
+++ ObjectPractice/Sword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Sword
+    {
+        public string TwoHanded { get; set; }
+        public string OneHanded { get; set; }
+        public string DualBladed { get; set; }
+
+        public void TwoHands(string swordName)
+        {
+            this.TwoHanded = swordName;
+        }
+        public void OneHand(string bladeName)
+        {
+            this.OneHanded = bladeName;
+        }
+        public void DualWield(string dualNames)
+        {
+            this.DualBladed = dualNames;
+        }
+    }
+}
